4cb3cd0b4d23f2b8b8947c62396b8d6aecdbc1dc
Rhaniel Xavier <rhanielmag.xavier@gmail.com>
2023-01-13 01:50:20 -0300

    add: Chapter 1 - Node Intro

---
+++ 01-fundamentos-nodejs/src/routes.js
@@ -0,0 +1,65 @@
+import { Database } from "./database.js"
+import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from "../utils/build-route-path.js"
+
+const database = new Database()
+
+export const routes = [
+    {
+        method: 'GET',
+        path: buildRoutePath('/users'),
+        handler: (req, res) => {
+            const search = req.query ? ({
+                name: req.query.search,
+                email: req.query.search
+            }) : (
+                undefined
+            )
+            const users = database.select('users', search)
+            return res.end(JSON.stringify(users))
+        }
+    },
+    {
+        method: 'POST',
+        path: buildRoutePath('/users'),
+        handler: (req, res) => {
+            const { name, email } = req.body
+
+            const user = {
+                id: randomUUID(),
+                name: name, 
+                email: email
+            }
+
+            database.insert('users',user)
+
+            return res.writeHead(201).end()
+        }
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const {id} = req.params
+
+            database.delete('users',id)
+
+            return res.writeHead(204).end()
+        }
+    },
+    {
+        method: 'PUT',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const {id} = req.params
+            const { name, email} = req.body
+
+            database.update('users',id, {
+                name,
+                email
+            })
+
+            return res.writeHead(204).end()
+        }
+    }
+]
